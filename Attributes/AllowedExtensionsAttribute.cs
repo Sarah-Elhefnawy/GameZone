b500c915d72436bf93a1a14db699893fdaa3c523
Sarah Elhefnawy <sarahelhefnawy0@gmail.com>
2024-09-02 01:59:12 +0300

    Trying out

---
+++ Attributes/AllowedExtensionsAttribute.cs
@@ -0,0 +1,44 @@
+﻿namespace GameZone.Attributes
+{
+	public class AllowedExtensionsAttribute:ValidationAttribute
+	{
+		//here i will add the validations by myself
+		//the extensions will not be static but will be expected as parameters
+
+		//private field that will expect the data that is allowed
+		private readonly string _allowedExtensions;
+
+        public AllowedExtensionsAttribute(string allowedExtensions)
+        {
+            _allowedExtensions = allowedExtensions;
+        }
+
+		protected override ValidationResult? IsValid
+			(object? value, ValidationContext validationContext)
+		{
+			//value is object--> i need to to be IFormFile
+			var file = value as IFormFile;
+
+			if (file is not null)
+			{
+				//i need to compare the extension if is valid
+				var extension = Path.GetExtension (file.FileName);
+				//now i have the extension
+
+				//var isAllowed = _allowedExtensions.Contains (extension);   ----> not right
+				//cuz i wrote the allowed extensions as one string with commas
+				//i need to convert it to array of strings, each element is an extension
+
+				var isAllowed = _allowedExtensions.Split(separator:',').Contains(extension, StringComparer.OrdinalIgnoreCase);
+				//StringComparer.OrdinalIgnoreCase---> cuz if the file is written as the extention is capitalized but extensions are all small(ex:jpeg)
+
+				if(!isAllowed)
+				{
+					return new ValidationResult(errorMessage: $"Only {_allowedExtensions} are allowed!");
+				}
+			}
+			return ValidationResult.Success;
+		}
+
+	}
+}
---
+++ ViewModel/GameFormViewModel.cs
@@ -0,0 +1,25 @@
+﻿namespace GameZone.ViewModel
+{
+	public class GameFormViewModel
+	{
+		[MaxLength(length: 250)]
+		public string Name { get; set; } = string.Empty;
+
+		[Display(Name = "Category")]
+		public int CategoryId { get; set; }
+
+		public IEnumerable<SelectListItem> Categories { get; set; } = Enumerable.Empty<SelectListItem>();
+		//IEnumerable as the user won't choose from integers and send it to the database 
+		//the user choose one string item from the drop down list or select list
+
+		[Display(Name = "Supported Devices")]
+		public List<int> SelectedDevices { get; set; } = default!;
+
+		public IEnumerable<SelectListItem> Devices { get; set; } = Enumerable.Empty<SelectListItem>();
+		//IEnumerable as the user won't choose from list of integers and send them to the database 
+		//the user choose multible string items from the drop down list or select list
+
+		[MaxLength(length: 2500)]
+		public string Description { get; set; } = string.Empty;
+	}
+}
