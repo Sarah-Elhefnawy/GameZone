e6457bf4218bd35ef2a9fa9ae0b06978b8483fe1
Sarah Elhefnawy <sarahelhefnawy0@gmail.com>
2024-09-06 00:01:05 +0300

    trying

--- obj/Debug/net8.0/GameZone.AssemblyInfo.cs
+++ obj/Debug/net8.0/GameZone.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameZone")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e479becb57b0e94f726b50bbfc22e4683213108")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b500c915d72436bf93a1a14db699893fdaa3c523")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameZone")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameZone")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
